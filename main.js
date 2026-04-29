7edb96e8ee17981fdc8c50ffaac3be2c28ca60f1
blaizeian <ianblaize100@gmail.com>
2026-04-29 12:55:18 +0300

    Connected frontend to Render backend

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ if (form) {
         };
 
         try {
-            const response = await fetch('http://127.0.0.1:8000/send-email', {
+            const response = await fetch('https://portfolio-backend-zhwg.onrender.com', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(formData)
